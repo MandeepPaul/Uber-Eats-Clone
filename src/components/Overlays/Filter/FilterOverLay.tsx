f098e761b6af97fea162a20dc245b209d8103a26
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2023-11-26 15:26:02 -0500

    Fixed filter overlay

--- src/components/Overlays/Filter/FilterOverLay.tsx
+++ src/components/Overlays/Filter/FilterOverLay.tsx
@@ -5,17 +5,26 @@ import { crossIcon } from "../../../SVG/svgIcon";
 
 const FilterOverlay: React.FC<{ onReset: () => void }> = ({ onReset }) => {
   return (
-    <Modal modal="top-0 left-0 p-5 w-full" reset={onReset}>
-      <div className="relative h-screen">
-        <Button onClick={onReset}>{crossIcon}</Button>
-        <h1 className="text-3xl font-bold pt-4">Filters</h1>
-        <Filter />
-        <Button
-          className="bg-black w-full absolute bottom-8 py-5 rounded-md text-xl text-white hover:opacity-75"
-          onClick={onReset}
-        >
-          Done
+    <Modal modal="top-0 left-0 h-screen w-full overflow-y-auto" reset={onReset}>
+      <div className="fixed z-40 p-3 border-b-2 border-gray-300 bg-white w-full">
+        <Button onClick={onReset} className="bg-white p-2 rounded-full">
+          {crossIcon}
         </Button>
+      </div>
+
+      <div className="p-5 my-5 mb-[100px] h-full">
+        <h1 className="text-4xl font-semibold pt-4">Filters</h1>
+
+        <Filter />
+
+        <div className="flex justify-center bg-white">
+          <Button
+            className="fixed bottom-0 bg-black w-[94%] py-4 my-6 rounded-md text-xl text-white hover:opacity-75"
+            onClick={onReset}
+          >
+            Done
+          </Button>
+        </div>
       </div>
     </Modal>
   );
